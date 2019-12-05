33fa0941898b5f3328b70e837430c570bbfaae73
zhuangjun1981 <wood_stocker@hotmail.com>
2019-12-05 11:42:51 -0800

    start of the package

---
+++ setup.py
@@ -0,0 +1,65 @@
+__author__ = 'junz'
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+import io
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+os.chdir(here)
+
+package_root = 'NeuroAnalysisTools'
+
+# get install requirements
+with open('requirements.txt') as req_f:
+    install_reqs = req_f.read().splitlines()
+install_reqs = [ir for ir in install_reqs if '#' not in ir]
+install_reqs = install_reqs[::-1]
+print('\ninstall requirements:')
+print('\n'.join(install_reqs))
+print('')
+
+# get long_description
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+# find version
+def find_version(f_path):
+    version_file = codecs.open(f_path, 'r').read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+version = find_version(os.path.join(here, 'NeuroAnalysisTools', '__init__.py'))
+
+setup(
+    name='NeuroAnalysisTools',
+    version=version,
+    python_requires='>3.0',
+    url='https://github.com/zhuangjun1981/NeuroAnalysisTools.git',
+    author='Jun Zhuang',
+    install_requires=install_reqs,
+    author_email='junz@alleninstitute.org',
+    description='Data analysis tools for systems neuroscience',
+    long_description=long_description,
+    packages=prepend_find_packages('NeuroAnalysisTools'),
+    include_package_data=True,
+    package_data={'':['*.md', '*.txt', '*.cfg', '*.hdf5']},
+    platforms='any',
+    classifiers=[
+        'Programming Language :: Python',
+        'Development Status :: 4 - Beta',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        ],
+)
