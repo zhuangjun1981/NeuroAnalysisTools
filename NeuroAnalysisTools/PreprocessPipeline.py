66ed6a3fbc289d18424a50dc9839952cda354d48
Jun Zhuang <wood_stocker@hotmail.com>
2020-02-14 11:00:49 -0800

    added generate_downsampled_small_movies

--- NeuroAnalysisTools/PreprocessPipeline.py
+++ NeuroAnalysisTools/PreprocessPipeline.py
@@ -670,3 +670,59 @@ class Preprocessor(object):
 
         print('\nDone!')
 
+    @staticmethod
+    def generate_downsampled_small_movies(data_folder,
+                                          save_folder,
+                                          identifier,
+                                          xy_downsample_rate=2,
+                                          t_downsample_rate=10,
+                                          channel_names=('green', 'red')):
+
+        """
+        mostly this will be used to heavily downsample motion corrected movies
+        for visual inspection.
+
+        :param data_folder:
+        :param save_folder:
+        :param identifier:
+        :param xy_downsample_rate:
+        :param t_downsample_rate:
+        :param channel_names:
+        :return:
+        """
+
+        plane_ns = [f for f in os.listdir(data_folder) if
+                    os.path.isdir(os.path.join(data_folder, f)) and f[:5] == 'plane']
+        plane_ns.sort()
+        print('planes:')
+        print('\n'.join(plane_ns))
+
+        for plane_n in plane_ns:
+            print('\nprocessing plane: {}'.format(plane_n))
+
+            save_folder = os.path.join(save_folder, plane_n)
+            if not os.path.isdir(save_folder):
+                os.makedirs(save_folder)
+
+            for ch_n in channel_names:
+                print('\n\tprocessing channel: {}'.format(ch_n))
+                plane_folder = os.path.join(data_folder, plane_n, ch_n, 'corrected')
+
+                # f_ns = [f for f in os.listdir(plane_folder) if f[-14:] == '_corrected.tif']
+                f_ns = [f for f in os.listdir(plane_folder) if f[-4:] == '.tif' and identifier in f]
+                f_ns.sort()
+                print('\t\t' + '\n\t\t'.join(f_ns) + '\n')
+
+                mov_d = []
+
+                for f_n in f_ns:
+                    print('\t\tprocessing {} ...'.format(f_n))
+                    curr_mov = tf.imread(os.path.join(plane_folder, f_n))
+                    curr_mov_d = ia.rigid_transform_cv2(img=curr_mov, zoom=(1. / xy_downsample_rate))
+                    curr_mov_d = ia.z_downsample(curr_mov_d, downSampleRate=t_downsample_rate, is_verbose=False)
+                    mov_d.append(curr_mov_d)
+
+                mov_d = np.concatenate(mov_d, axis=0)
+                save_n = '{}_{}_{}_downsampled.tif'.format(os.path.split(data_folder)[1], plane_n, ch_n)
+                tf.imsave(os.path.join(save_folder, save_n), mov_d)
+
