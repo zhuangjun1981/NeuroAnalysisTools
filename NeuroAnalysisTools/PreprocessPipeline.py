6c39c4250519f862f2f825437558d67893ace683
Jun Zhuang <wood_stocker@hotmail.com>
2020-02-14 10:28:06 -0800

    Added motion correction function

--- NeuroAnalysisTools/PreprocessPipeline.py
+++ NeuroAnalysisTools/PreprocessPipeline.py
@@ -6,6 +6,7 @@ import NeuroAnalysisTools.core.ImageAnalysis as ia
 import NeuroAnalysisTools.core.PlottingTools as pt
 import matplotlib.pyplot as plt
 import cv2
+import NeuroAnalysisTools.MotionCorrection as mc
 
 
 class Preprocessor(object):
@@ -440,3 +441,72 @@ class Preprocessor(object):
             print('\n\tDone!')
         else:
             raise LookupError('Do not understand scope: "{}". Should be "sutter" or "deepscope".'.format(scope))
+
+    @staticmethod
+    def motion_correction(data_folder,
+                          reference_channel_name,
+                          apply_channel_names):
+        """
+
+        :param data_folder: str, path to the reorganized folder
+        :param reference_channel_name: str, channel name for correction reference
+        :param apply_channel_names: list of str, channel names for applying correction offsets
+        """
+
+        def correct(data_folder, ref_ch_n, apply_ch_ns):
+            # ref_ch_n = 'green'
+            # apply_ch_ns = ['green', 'red']
+
+            curr_folder = os.path.dirname(os.path.realpath(__file__))
+            os.chdir(curr_folder)
+
+            ref_data_folder = os.path.join(data_folder, ref_ch_n)
+
+            mc.motion_correction(input_folder=ref_data_folder,
+                                 input_path_identifier='.tif',
+                                 process_num=6,
+                                 output_folder=os.path.join(ref_data_folder, 'corrected'),
+                                 anchor_frame_ind_chunk=10,
+                                 anchor_frame_ind_projection=0,
+                                 iteration_chunk=10,
+                                 iteration_projection=10,
+                                 max_offset_chunk=(100., 100.),
+                                 max_offset_projection=(100., 100.),
+                                 align_func=mc.phase_correlation,
+                                 preprocessing_type=6,
+                                 fill_value=0.)
+
+            offsets_path = os.path.join(ref_data_folder, 'corrected', 'correction_offsets.hdf5')
+            ref_fns = [f for f in os.listdir(ref_data_folder) if f[-4:] == '.tif']
+            ref_fns.sort()
+            ref_paths = [os.path.join(ref_data_folder, f) for f in ref_fns]
+            print('\nreference paths:')
+            print('\n'.join(ref_paths))
+
+            for apply_ch_i, apply_ch_n in enumerate(apply_ch_ns):
+                apply_data_folder = os.path.join(data_folder, apply_ch_n)
+                apply_fns = [f for f in os.listdir(apply_data_folder) if f[-4:] == '.tif']
+                apply_fns.sort()
+                apply_paths = [os.path.join(apply_data_folder, f) for f in apply_fns]
+                print('\napply paths:')
+                print('\n'.join(apply_paths))
+
+                mc.apply_correction_offsets(offsets_path=offsets_path,
+                                            path_pairs=zip(ref_paths, apply_paths),
+                                            output_folder=os.path.join(apply_data_folder, 'corrected'),
+                                            process_num=6,
+                                            fill_value=0.,
+                                            avi_downsample_rate=10,
+                                            is_equalizing_histogram=False)
+
+        plane_folders = [f for f in os.listdir(data_folder) if f[0:5] == 'plane' and
+                         os.path.isdir(os.path.join(data_folder, f))]
+        plane_folders.sort()
+        print('folders to be corrected:')
+        print('\n'.join(plane_folders))
+
+        for plane_folder in plane_folders:
+            correct(os.path.join(data_folder, plane_folder),
+                    ref_ch_n=reference_channel_name,
+                    apply_ch_ns=apply_channel_names)
+
