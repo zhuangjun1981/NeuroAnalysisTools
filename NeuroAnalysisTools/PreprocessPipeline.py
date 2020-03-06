591d49bf70317fdca3d299976749c46a897332f3
Jun Zhuang <wood_stocker@hotmail.com>
2020-03-06 15:10:33 -0800

    more functions

--- NeuroAnalysisTools/PreprocessPipeline.py
+++ NeuroAnalysisTools/PreprocessPipeline.py
@@ -160,7 +160,7 @@ def downsample_mov(nwb_path, dset_path, dr, chunk_size, process_num):
     return np.concatenate(mov_d, axis=0)
 
 
-def correct_single_stack_step(param):
+def motion_correction_single_stack_step(param):
 
     """for multiprocessing"""
 
@@ -200,6 +200,23 @@ def correct_single_stack_step(param):
                                     fill_value=fill_value,
                                     avi_downsample_rate=avi_downsample_rate,
                                     is_equalizing_histogram=is_equalizing_histogram)
+
+
+def apply_offsets_single_stack_step(param):
+
+    movie_path, offsets_path = param
+
+    offsets_f = h5py.File(offsets_path)
+    ref_path = offsets_f['file_0000'].attrs['path']
+    offsets_f.close()
+
+    mc.apply_correction_offsets(offsets_path=offsets_path,
+                                path_pairs=[[ref_path, movie_path]],
+                                output_folder=os.path.split(os.path.realpath(movie_path))[0],
+                                process_num=1,
+                                fill_value=0.,
+                                avi_downsample_rate=None,
+                                is_equalizing_histogram=False)
 
 
 class Preprocessor(object):
@@ -569,8 +586,8 @@ class Preprocessor(object):
         print('\tDone.')
 
     @staticmethod
-    def motion_correction_unaveraged_zstack_files(data_folder, reference_channel_name,
-                                                  process_num):
+    def motion_correction_unaveraged_zstack(data_folder, reference_channel_name,
+                                            process_num):
 
 
 
@@ -598,8 +615,29 @@ class Preprocessor(object):
                            fill_value, is_apply, avi_downsample_rate, is_equalizing_histogram))
 
         chunk_p = Pool(process_num)
-        chunk_p.map(correct_single_stack_step, params)
+        chunk_p.map(motion_correction_single_stack_step, params)
         print('\tDone.')
+
+    @staticmethod
+    def apply_offsets_unaveraged_zstack(data_folder, reference_channel_name, apply_channel_names,
+                                        process_num):
+
+
+        step_ns = [f for f in os.listdir(os.path.join(data_folder, reference_channel_name))
+                   if os.path.isdir(os.path.join(data_folder, reference_channel_name, f))]
+        step_ns.sort()
+
+        chunk_p = Pool(process_num)
+
+        for ch_n in apply_channel_names:
+            mc_params = []
+            for step_n in step_ns:
+                movie_path = os.path.join(data_folder, ch_n, step_n, step_n + '.tif')
+                offsets_path = os.path.join(data_folder, reference_channel_name,
+                                            step_n, 'correction_offsets.hdf5')
+                mc_params.append((movie_path, offsets_path))
+
+            chunk_p.map(apply_offsets_single_stack_step, mc_params)
 
     @staticmethod
     def motion_correction_zstack_all_steps(data_folder, save_folder, identifier,
