c15f4d010db78882f2cb96dba303aba2c8619463
Jun Zhuang <wood_stocker@hotmail.com>
2020-07-15 14:56:34 -0700

    updated a script

--- NeuroAnalysisTools/scripts/analysis_pipeline_zstack/000_preprocessing_zstack_averaged.py
+++ NeuroAnalysisTools/scripts/analysis_pipeline_zstack/000_preprocessing_zstack_averaged.py
@@ -1,80 +1,72 @@
 import os
 import NeuroAnalysisTools.PreprocessPipeline as pp
 
-date = '200227'
-mid = '500578'
+date = '200715'
+mid = '533920'
 scope = 'sutter'
 
-vasmap_2p_chs = ('green', 'red')
-zstack_chs = ('green', 'red')
-ch_rf = 'red'
-chs_apply = ('green', 'red')
+identifier = 'zstack_zoom2'
+vasmap_2p_chs = ('green',)
+zstack_chs = ('green',)
+ch_rf = 'green'
+chs_apply = ('green',)
 
-data_folder = r"\\allen\programs\braintv\workgroups\nc-ophys\Jun\raw_data"
+source_folder = r"\\allen\programs\braintv\workgroups\nc-ophys\Jun\raw_data"
 
 curr_folder = os.path.dirname(os.path.realpath(__file__))
 os.chdir(curr_folder)
 
-ppr = pp.Preprocessor()
-
 if scope == 'sutter':
-    folder_suffix = '2p'
-elif scope == 'deepscope' or scope == 'DeepScope':
-    folder_suffix = 'deepscope'
+    sess_folder = os.path.join(source_folder, '{}-M{}-2p'.format(date, mid))
+    vasmap_wf_identifier = 'JCam'
+elif scope == 'deepscope':
+    sess_folder = os.path.join(source_folder, '{}-M{}-deepscope'.format(date, mid))
+    vasmap_wf_identifier = 'vasmap_wf'
 else:
     raise LookupError('Do not understand scope type.')
 
-sess_folder = os.path.join(data_folder, '{}-M{}-{}'.format(date, mid, folder_suffix))
+ppr = pp.Preprocessor()
 
-# ppr.get_vasmap_wf(data_folder=os.path.join(sess_folder, 'vasmap_wf_green'),
-#                   save_folder=curr_folder,
-#                   scope=scope, identifier='JCamF',
-#                   save_suffix='_green')
+# # get 2p vasculature maps as tif
+# _ = ppr.get_vasmap_2p(data_folder=os.path.join(sess_folder, 'vasmap_2p'),
+#                       scope=scope,
+#                       channels=vasmap_2p_chs,
+#                       identifier='vasmap_2p',
+#                       is_equalize=False,
+#                       save_folder=curr_folder)
 #
-# ppr.save_png_vasmap(tif_path='vasmap_wf_rotated_green.tif',
-#                     prefix='{}-M{}'.format(date, mid),
-#                     saturation_level=5.)
+# # save aligned 2p vasculature maps as png
+# for vasmap_2p_ch in vasmap_2p_chs:
+#     ppr.save_png_vasmap(tif_path=os.path.join(curr_folder, 'vasmap_2p_{}_rotated.tif'.format(vasmap_2p_ch)),
+#                         prefix='{}_{}'.format(date, mid), saturation_level=10)
 #
-# ppr.get_vasmap_wf(data_folder=os.path.join(sess_folder, 'vasmap_wf_red'),
-#                   save_folder=curr_folder,
-#                   scope=scope, identifier='JCamF',
-#                   save_suffix='_red')
+# # get widefield vasculature maps as tif
+# _ = ppr.get_vasmap_wf(data_folder=os.path.join(sess_folder, 'vasmap_wf'),
+#                       save_folder=curr_folder,
+#                       scope=scope,
+#                       identifier=vasmap_wf_identifier)
 #
-# ppr.save_png_vasmap(tif_path='vasmap_wf_rotated_red.tif',
-#                     prefix='{}-M{}'.format(date, mid),
-#                     saturation_level=5.)
+# # save aligned widefield vasculature maps as png
+# ppr.save_png_vasmap(tif_path=os.path.join(curr_folder, 'vasmap_wf_rotated.tif'),
+#                     prefix='{}_{}'.format(date, mid), saturation_level=5)
 #
-# ppr.get_vasmap_2p(data_folder=sess_folder, save_folder=curr_folder,
-#                   scope=scope, channels=vasmap_2p_chs, identifier='vasmap_2p',
-#                   is_equalize=False)
 #
-# for vmch in vasmap_2p_chs:
-#     ppr.save_png_vasmap(tif_path='vasmap_2p_{}_rotated.tif'.format(vmch),
-#                         prefix='{}-M{}'.format(date, mid), saturation_level=10.)
-
-ppr.split_channels(data_folder=sess_folder, save_folder=curr_folder,
-                   identifier='zstack_zoom1', channels=zstack_chs)
-
-ppr.split_channels(data_folder=sess_folder, save_folder=curr_folder,
-                   identifier='zstack_zoom2', channels=zstack_chs)
-
-ppr.motion_correction_zstack(data_folder=curr_folder, save_folder=curr_folder,
-                             identifier='zstack_zoom1',
-                             reference_channel_name=ch_rf,
-                             apply_channel_names=chs_apply)
-
-ppr.motion_correction_zstack(data_folder=curr_folder, save_folder=curr_folder,
-                             identifier='zstack_zoom2',
-                             reference_channel_name=ch_rf,
-                             apply_channel_names=chs_apply)
-
-ppr.transform_images(data_folder=curr_folder, save_folder=curr_folder,
-                     identifiers=['zstack', 'corrected'],
-                     scope=scope)
-
-ppr.save_projections_as_png_files(data_folder=curr_folder, save_folder=curr_folder,
-                                  identifiers=['zstack', 'corrected', 'aligned'],
-                                  save_prefix='{}-M{}'.format(date, mid),
-                                  projection_type='max', saturation_level=5.)
+# ppr.split_channels(data_folder=os.path.join(sess_folder, identifier),
+#                    save_folder=curr_folder,
+#                    identifier=identifier, channels=zstack_chs)
+#
+# ppr.motion_correction_zstack_all_steps(data_folder=curr_folder, save_folder=curr_folder,
+#                                        identifier=identifier,
+#                                        reference_channel_name=ch_rf,
+#                                        apply_channel_names=chs_apply)
+#
+# ppr.transform_images(data_folder=curr_folder, save_folder=curr_folder,
+#                      identifiers=[identifier, 'corrected'],
+#                      scope=scope)
+#
+# ppr.save_projections_as_png_files(data_folder=curr_folder, save_folder=curr_folder,
+#                                   identifiers=[identifier, 'corrected', 'aligned'],
+#                                   save_prefix='{}-M{}'.format(date, mid),
+#                                   projection_type='max', saturation_level=5.)
 
 
