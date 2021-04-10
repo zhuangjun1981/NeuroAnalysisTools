60c6b49ca47033d0e903d83b79312ef8dfed4d74
Jun Zhuang <wood_stocker@hotmail.com>
2021-04-09 20:05:44 -0700

    small changes

--- NeuroAnalysisTools/SingleCellAnalysis.py
+++ NeuroAnalysisTools/SingleCellAnalysis.py
@@ -362,17 +362,12 @@ def get_local_similarity_index(mask1, mask2):
     square1 = np.sum((mask1 * mask1).flat)
     square2 = np.sum((mask2 * mask2).flat)
 
-    print(square1)
-    print(square2)
-
     if square1 == 0 or square2 == 0.:
         return np.nan
     else:
         value1 = np.sum((mask1 * mask2).flat)
         value2 = np.sqrt(square1 * square2)
 
-        print(value1)
-        print(value2)
         return value1 / value2
 
 
